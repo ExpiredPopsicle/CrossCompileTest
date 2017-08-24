c23db40e120eb8a6a2a8ab5d1eeb04013fdedf41
ExpiredPopsicle <expiredpopsicle@gmail.com>
2017-08-23 18:22:44 -0700

    Stuff compiles.

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-    printf("Test: %d\n", getGeneratedValue());
+    // printf("Test: %d\n", getGeneratedValue());
+    printf("Dumb test first\n");
     return 0;
 }
