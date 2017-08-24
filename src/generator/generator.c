0e2e2dcddce6993ea2fcb39a1c210f7b5f57fb7c
ExpiredPopsicle <expiredpopsicle@gmail.com>
2017-08-23 18:33:27 -0700

    Attempting to add AX_CC_FOR_BUILD stuff.

---
+++ src/generator/generator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *generated = fopen("generated.c", "w+");
+    if(!generated) {
+        fprintf(stderr, "Could not open generated.c for writing\n");
+        return 1;
+    }
+
+    fprintf(generated, "int generatedValue() { return 1234; }\n");
+
+    fclose(generated);
+
+    return 0;
+}
+
