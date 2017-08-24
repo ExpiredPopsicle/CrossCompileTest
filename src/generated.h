8cbe6cdd9b87af7824153d2f0422c5d6495d511a
ExpiredPopsicle <expiredpopsicle@gmail.com>
2017-08-23 18:20:01 -0700

    I haven't even tried compiling yet.

---
+++ src/generated.h
@@ -0,0 +1,6 @@
+#ifndef GENERATED_H
+#define GENERATED_H
+
+int getGeneratedValue(void);
+
+#endif // GENERATED_H
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include <generated.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Test: %d\n", getGeneratedValue());
+    return 0;
+}
